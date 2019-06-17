3e7eb4fcfa0c3b1e32d80d7229709a98a5804699
Lab4 User4 <jrigsbee+lab4@redhat.com>
2019-06-17 16:56:43 +0100

    Exercise 3 completed

--- components/exercises/03_props.js
+++ components/exercises/03_props.js
@@ -1,10 +1,37 @@
 import React , { Component } from 'react';
 import '../../node_modules/bootstrap/dist/css/bootstrap.css';
+import Table from 'react-bootstrap/Table'
 
 export default class CourseModulesWithProps extends Component { 
     render() {
+        const thName = "Name";
+        const thLectures = "No. Lectures"
+        const thPracticals = "No. Practicals"
         return (
-            <h1> TODO </h1> 
+            <div>
+              <h1>{this.props.course}</h1>
+              <Table striped bordered hover variant="dark">
+                <thead>
+                  <tr>
+                    <th>{thName}</th>
+                    <th>{thLectures}</th>
+                    <th>{thPracticals}</th>
+                  </tr>
+                </thead>
+                <tbody >
+                <tr>
+                <td>{this.props.modules[0].name}</td>
+                <td>{this.props.modules[0].noLectures}</td>
+                <td>{this.props.modules[0].noPracticals}</td>
+                </tr>
+                <tr>
+                <td>{this.props.modules[1].name}</td>
+                <td>{this.props.modules[1].noLectures}</td>
+                <td>{this.props.modules[1].noPracticals}</td>
+                </tr>
+                </tbody>
+              </Table>
+            </div>
           );
     }
 }
