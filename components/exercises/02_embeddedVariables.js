0c881bfc6ec2d5a961fae4312ff84b57c4cfd46a
Lab4 User4 <jrigsbee+lab4@redhat.com>
2019-06-17 15:46:24 +0100

    Exercise 2 completed

--- components/exercises/02_embeddedVariables.js
+++ components/exercises/02_embeddedVariables.js
@@ -1,8 +1,71 @@
 import React , { Component } from 'react';
 import '../../node_modules/bootstrap/dist/css/bootstrap.css';
+import Table from 'react-bootstrap/Table'
 
 export default class CourseModulesEmbeddedVars extends Component { 
     render() {
-        return <h1> TODO </h1>
-    }
+        const courseTile = "HDip Computer Science - Modules table.";
+        const thName = "Name";
+        const thLectures = "No. Lectures"
+        const thPracticals = "No. Practicals"
+        const modules = [
+        {
+            name: "DevOps",
+            noLectures: 2,
+            noPracticals: 2
+        },
+        {
+            name: "Enterprise Web Dev",
+            noLectures: 3,
+            noPracticals: 2
+        },
+        {
+            name: "Database Systems",
+            noLectures: 2,
+            noPracticals: 2
+          },
+          {
+            name: "Enterprise Web Dev",
+            noLectures: 2,
+            noPracticals: 3
+          }
+        ];
+        return (
+            <div>
+              <h1>{courseTile}</h1>
+              <Table striped bordered hover variant="dark">
+                <thead>
+                  <tr>
+                    <th>{thName}</th>
+                    <th>{thLectures}</th>
+                    <th>{thPracticals}</th>
+                  </tr>
+                </thead>
+                <tbody >
+                <tr>
+                <td>{modules[0].name}</td>
+                <td>{modules[0].noLectures}</td>
+                <td>{modules[0].noPracticals}</td>
+                </tr>
+                <tr>
+                <td>{modules[1].name}</td>
+                <td>{modules[1].noLectures}</td>
+                <td>{modules[1].noPracticals}</td>
+                </tr>
+                <tr>
+                <td>{modules[2].name}</td>
+                <td>{modules[2].noLectures}</td>
+                <td>{modules[2].noPracticals}</td>
+                </tr>
+                <tr>
+                <td>{modules[3].name}</td>
+                <td>{modules[3].noLectures}</td>
+                <td>{modules[3].noPracticals}</td>
+                </tr>
+                </tbody >
+              </Table>
+            </div>
+          );
+      
+        }
 }
