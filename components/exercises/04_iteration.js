8df91c41e73ba8be20f864f4fbb7e956f20bf073
Lab4 User4 <jrigsbee+lab4@redhat.com>
2019-06-18 09:27:19 +0100

    Exercise 4 completed

--- components/exercises/04_iteration.js
+++ components/exercises/04_iteration.js
@@ -1,10 +1,34 @@
-import React , { Component } from 'react';
-import '../../node_modules/bootstrap/dist/css/bootstrap.css';
+import React, { Component } from 'react';
+import Table from 'react-bootstrap/Table'
 
-export default class CourseModulesCollections extends Component { 
+export default class CourseModulesCollections extends Component {
     render() {
+
+        let result = this.props.modules.map((listModules, index) => {
+            return (
+                <tr key={index}>
+                    <td>{listModules.name}</td>
+                    <td>{listModules.noLectures}</td>
+                    <td>{listModules.noPracticals}</td>
+                </tr>
+            );
+        });
         return (
-            <h1> TODO </h1> 
-          );
+            <div>
+                <h1>{this.props.course} </h1>
+                <Table striped bordered hover variant="dark">
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>No. Lectures</th>
+                            <th>No. Practicals</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {result}
+                    </tbody>
+                </Table>
+            </div>
+        );
     }
 }
